7fb40f451807c48ae3bb784a03737454ea6a902e
Friebay <frybaylt@gmail.com>
2024-09-09 22:11:05 +0300

    Medianos radimo funkcija

--- Obj/Obj.cpp
+++ Obj/Obj.cpp
@@ -11,10 +11,33 @@ struct Duomenys {
     int egz;
     int paz_suma = 0;
     float vidurkis = 0;
+    float mediana = 0;
 };
 
-int main()
-{
+void rikiavimas(int pazymiu_sarasas[], int pazymiu_kiekis) {
+    for (int i = 0; i < pazymiu_kiekis - 1; i++) {
+        for (int j = i + 1; j < pazymiu_kiekis; j++) {
+            if (pazymiu_sarasas[i] > pazymiu_sarasas[j]) {
+                int temp = pazymiu_sarasas[i];
+                pazymiu_sarasas[i] = pazymiu_sarasas[j];
+                pazymiu_sarasas[j] = temp;
+            }
+        }
+    }
+}
+
+float medianosRadimas(int pazymiu_sarasas[], int pazymiu_kiekis) {
+    rikiavimas(pazymiu_sarasas, pazymiu_kiekis);
+    if (pazymiu_kiekis % 2 != 0) {
+        return pazymiu_sarasas[pazymiu_kiekis / 2];
+    }
+
+    else {
+        return (pazymiu_sarasas[pazymiu_kiekis / 2 - 1] + pazymiu_sarasas[pazymiu_kiekis / 2]) / 2.0;
+    }
+}
+
+int main() {
     Duomenys stud1, stud2;
 
     cout << "Vardas: ";
@@ -26,12 +49,13 @@ int main()
     for (int i = 0; i < 5; i++) {
         cout << "Pazymys: ";
         cin >> stud1.paz[i];
-
         stud1.paz_suma += stud1.paz[i];
     }
 
     stud1.vidurkis = float((stud1.paz_suma) / 5);
-    cout << stud1.Vardas << " " << stud1.Pavarde << " Vidurkis: " << stud1.vidurkis << endl;
+    stud1.mediana = medianosRadimas(stud1.paz, 5);
+
+    cout << stud1.Vardas << " " << stud1.Pavarde << " Vidurkis: " << stud1.vidurkis << " Mediana: " << stud1.mediana << endl;
 
     cout << "Vardas: ";
     cin >> stud2.Vardas;
@@ -42,12 +66,13 @@ int main()
     for (int i = 0; i < 5; i++) {
         cout << "Pazymys: ";
         cin >> stud2.paz[i];
-
         stud2.paz_suma += stud2.paz[i];
     }
 
     stud2.vidurkis = float((stud2.paz_suma) / 5);
-    cout << stud2.Vardas << " " << stud2.Pavarde << " Vidurkis: " << stud2.vidurkis << endl;
+    stud2.mediana = medianosRadimas(stud2.paz, 5);
+
+    cout << stud2.Vardas << " " << stud2.Pavarde << " Vidurkis: " << stud2.vidurkis << " Mediana: " << stud2.mediana << endl;
 
     return 0;
 }
