013ab0ea1deddbefe48a6e439cff326ae05bb7c0
Friebay <frybaylt@gmail.com>
2024-10-22 16:44:14 +0300

    Seni atnaujinimai iš v0.2

--- ObjProg_Ver1-0.1/isskaidymas/compile.cmd
+++ ObjProg_Ver1-0.1/isskaidymas/compile.cmd
@@ -1,1 +1,2 @@
-g++ main.cpp funkcijos.cpp failo_apdorojimas.cpp -o program
+g++ -O3 -march=native -mtune=native -flto -funroll-loops main.cpp funkcijos.cpp failo_apdorojimas.cpp -o program
+pause
--- isskaidymas/compile.cmd
+++ isskaidymas/compile.cmd
@@ -1,2 +1,2 @@
-g++ main.cpp funkcijos.cpp failo_apdorojimas.cpp generuoti_failus.cpp -o program
+g++ -O3 -march=native -mtune=native -flto -funroll-loops main.cpp funkcijos.cpp failo_apdorojimas.cpp generuoti_failus.cpp -o program
 pause
